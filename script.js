aaa74c8131ce3b8dfb438ceeb9018ba3513fa107
Payal Bhandwalkar <bhandwalkar.payal@gmail.com>
2021-08-24 16:50:57 +0530

    commit

--- script.js
+++ script.js
@@ -33,3 +33,72 @@ function addNewAQField() {
     aqOb.insertBefore(newNode, aqAddButtonOb);
 
 }
+///generating resume
+
+function generateCV() {
+    console.log("generate");
+
+
+    let nameField = document.getElementById("nameField").value;
+   
+    let nameT1 = document.getElementById("nameT1");
+
+    nameT1.innerHTML = nameField;
+
+    //shortcut
+
+    document.getElementById('nameT2').innerHTML=nameField;
+
+    //conatct
+
+    document.getElementById("contactT").innerHTML = document.getElementById("contactField").value;
+
+    //address
+
+    document.getElementById("addressT").innerHTML = document.getElementById("addField").value;
+
+    //links
+
+    document.getElementById("fbT").innerHTML = document.getElementById("fbField").value;
+
+    document.getElementById("gitT").innerHTML = document.getElementById("gitField").value;
+
+    document.getElementById("linkedT").innerHTML = document.getElementById("linkedinField").value;
+
+
+    //objective
+
+    document.getElementById('objectiveT').innerHTML = document.getElementById("objectiveField").value;
+
+    //we
+
+    let wes = document.getElementsByClassName("weField")
+
+    let str =''
+
+    for(let e of wes)
+    {
+        str=str + `<li> ${e.value} <li>`;
+    }
+    document.getElementById("weT").innerHTML = str;
+    
+    //aq
+
+    let aqs = document.getElementsByClassName("aqField");
+
+    let str1 = "";
+
+    for(let e of aqs) {
+        str1 = str1 + `<li> ${e.value} <li>`;
+    }
+    document.getElementById("aqT").innerHTML = str1;
+
+
+    document.getElementById("cv-form").style.display="none";
+    document.getElementById("cv-template").style.display="block";
+
+}
+//print 
+function printCV() {
+    window.print();
+}
